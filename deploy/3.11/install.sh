a9f619979b3798e3a48153985499b558324eb8ae
Igor <idyurev@gmail.com>
2020-06-03 16:04:49 +0300

    patch install script

--- deploy/3.11/install.sh
+++ deploy/3.11/install.sh
@@ -2,9 +2,9 @@
 
 oc adm new-project super-project-operator
 
-oc process -f install-template-3.11.yaml --local && \
-    -p GIT_REPO=$1 && \
-    -p GIT_CLONE_SECRET=$2 && \
+oc process -f install-template-3.11-private.yaml --local  \
+    -p GIT_REPO=$1  \
+    -p GIT_CLONE_SECRET=$2  \
     -p GIT_BRANCH=$3  | oc -n super-project-operator  apply -f-
 
 
