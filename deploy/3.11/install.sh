db32361dc065ba1861c33ac64adf37cea2afdea0
Igor <idyurev@gmail.com>
2020-06-03 15:24:55 +0300

    Start preparing for migration to 4.x

---
+++ deploy/3.11/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+oc adm new-project super-project-operator
+
+oc process -f install-template-3.11.yaml --local && \
+    -p GIT_REPO=$1 && \
+    -p GIT_CLONE_SECRET=$2 && \
+    -p GIT_BRANCH=$3  | oc -n super-project-operator  apply -f-
+
+
+
+
+
+
--- deploy/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-oc process -f deploy/install-template-3.11.yaml --local -p GIT_REPO=<OPERATOR_GIT_REPOSITORY_URL> | oc -n super-project-operator  apply -f-
-
-
-
-
-
-
